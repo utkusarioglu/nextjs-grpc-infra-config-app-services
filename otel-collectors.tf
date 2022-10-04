cb426493ee6655a1747699f335d0903cfd995d85
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-04 04:02:28 +0000

    Chore(otel): Prune otel config
    - Remove obsolete otel resource
    - Rename `otel-log.tf` to `otel-collectors.tf`. This is done because
      this resource provides config for all kinds of otel resources,
      not only log.

--- otel-collectors.tf
+++ otel-collectors.tf
@@ -1,7 +1,7 @@
-resource "helm_release" "otel_log" {
+resource "helm_release" "otel_collectors" {
   count             = 1
-  name              = "otel-log"
-  chart             = "${local.project_root_path}/otel-log"
+  name              = "otel-collectors"
+  chart             = "${local.project_root_path}/otel-collectors"
   dependency_update = true
   atomic            = var.helm_atomic
   namespace         = "observability"
--- otel-trace.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "otel_trace" {
-  count             = 0
-  name              = "otel-trace"
-  chart             = "${local.project_root_path}/otel-trace"
-  dependency_update = true
-  atomic            = var.helm_atomic
-  namespace         = "observability"
-  timeout           = var.helm_timeout_unit
-}
