cd36ae8e82985fab03c7e1216da32d9ffe57c8e4
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-07 16:40:12 +0000

    Chore(tier-1): Switch to tf only `otel_operator`
    - Remove the need for the repo for definition of `otel-operator`.
      Repo does not do much and shouldn't exist unless it gains a lot
      more config.

--- modules/tier-1/otel-operator.tf
+++ modules/tier-1/otel-operator.tf
@@ -1,11 +1,23 @@
 resource "helm_release" "otel_operator" {
   count             = 1
   name              = "otel-operator"
-  chart             = "${var.project_root_path}/otel-operator"
+  chart             = "opentelemetry-operator"
+  repository        = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  version           = "0.11.8"
   dependency_update = true
   atomic            = var.helm_atomic
   namespace         = "observability"
   timeout           = var.helm_timeout_unit * 3
+
+  values = [
+    yamlencode({
+      admissionWebhooks = {
+        certManager = {
+          enabled = true
+        }
+      }
+    })
+  ]
 
   depends_on = [
     helm_release.cert_manager[0]
